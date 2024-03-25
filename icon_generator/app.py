9ad2a751b1916bc048747d6d53aee5b0175c01b9
Winikka <winikkc@hsd.k12.or.us>
2024-03-25 09:47:52 -0700

    ref: moving ui code out of controller (2.0)

--- icon_generator/app.py
+++ icon_generator/app.py
@@ -1,5 +1,4 @@
 import sys
-import controller
 from PyQt6.QtGui import QPalette, QColor
 from PyQt6.QtSvgWidgets import QSvgWidget
 from PyQt6.QtWidgets import (
@@ -9,6 +8,7 @@ from PyQt6.QtWidgets import (
     QMainWindow,
     QPushButton,
     QGridLayout,
+    QVBoxLayout,
     QWidget,
 )
 
@@ -36,14 +36,14 @@ class MainWindow(QMainWindow):
         self.get_avatar_button = QPushButton("Get Avatar")
 
         # Icon button widgets
-        pixel_slot_layout = controller.get_icon_layout("pixel_art")
-        adventurer_slot_layout = controller.get_icon_layout("adventurer")
-        botts_slot_layout = controller.get_icon_layout("bottts")
-        croodles_slot_layout = controller.get_icon_layout("croodles")
-        identicon_slot_layout = controller.get_icon_layout("identicon")
-        lorelei_slot_layout = controller.get_icon_layout("lorelei")
-        rings_slot_layout = controller.get_icon_layout("rings")
-        shapes_slot_layout = controller.get_icon_layout("shapes")
+        pixel_slot_layout = self.get_icon_layout("pixel_art")
+        adventurer_slot_layout = self.get_icon_layout("adventurer")
+        botts_slot_layout = self.get_icon_layout("bottts")
+        croodles_slot_layout = self.get_icon_layout("croodles")
+        identicon_slot_layout = self.get_icon_layout("identicon")
+        lorelei_slot_layout = self.get_icon_layout("lorelei")
+        rings_slot_layout = self.get_icon_layout("rings")
+        shapes_slot_layout = self.get_icon_layout("shapes")
 
         # Add widgets to the layout
         layout.addWidget(title_label, 0, 0, 1, 4)
@@ -62,6 +62,26 @@ class MainWindow(QMainWindow):
         widget = QWidget()
         widget.setLayout(layout)
         self.setCentralWidget(widget)
+
+    def get_icon_layout(self, icon_type: str) -> QVBoxLayout:
+        """return a vbox layout with an avatar and label.
+
+        Arguments:
+            icon_type: the type of icon we will display.
+
+        Returns:
+            icon_layout: a vbox layout with an avatar and label.
+        """
+        icon_layout = QVBoxLayout()
+        folder = "resources/images/"
+        filepath = folder + icon_type + "_avatar.svg"
+        avatar_svg = QSvgWidget(filepath)
+        avatar_svg.setFixedSize(64, 64)
+        label = icon_type.replace("_", " ")
+        avatar_label = QLabel(label)
+        icon_layout.addWidget(avatar_svg)
+        icon_layout.addWidget(avatar_label)
+        return icon_layout
 
 
 class Color(QWidget):
--- icon_generator/controller.py
+++ icon_generator/controller.py
@@ -5,11 +5,7 @@ Python code to make API connections.
 """
 
 import requests as re
-from PyQt6.QtSvgWidgets import QSvgWidget
-from PyQt6.QtWidgets import (
-    QLabel,
-    QVBoxLayout,
-)
+
 
 # build out the URL based on API docs
 base_url = "https://api.dicebear.com/7.x/"
@@ -20,29 +16,9 @@ seed = "?seed=HundredVisionsGuy"
 url = base_url + style + filetype + seed
 response = re.get(url)
 
-
-def get_icon_layout(icon_type: str) -> QVBoxLayout:
-    """return a vbox layout with an avatar and label.
-
-    Arguments:
-        icon_type: the type of icon we will display.
-
-    Returns:
-        icon_layout: a vbox layout with an avatar and label.
-    """
-    icon_layout = QVBoxLayout()
-    folder = "resources/images/"
-    filepath = folder + icon_type + "_avatar.svg"
-    avatar_svg = QSvgWidget(filepath)
-    avatar_svg.setFixedSize(64, 64)
-    label = icon_type.replace("_", " ")
-    avatar_label = QLabel(label)
-    icon_layout.addWidget(avatar_svg)
-    icon_layout.addWidget(avatar_label)
-    return icon_layout
-
-
-if response.ok:
-    print(response.text)
-else:
-    print(f"There was an error: {response.status_code}")
+if __name__ == '__main__':
+    response = re.get(url)
+    if response.ok:
+        print(response.text)
+    else:
+        print(f"There was an error: {response.status_code}")
