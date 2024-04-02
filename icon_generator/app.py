c071a4c49def7e0c2faa594365b747b144e01127
Hundredvisionsguy <winikkc@hsd.k12.or.us>
2024-04-02 11:05:29 -0700

    part 3 done - next step custom Widget

--- icon_generator/app.py
+++ icon_generator/app.py
@@ -1,65 +1,74 @@
 import sys
 import controller
-from PyQt6.QtGui import QPalette, QColor
+from PyQt6.QtCore import Qt
 from PyQt6.QtSvgWidgets import QSvgWidget
 from PyQt6.QtWidgets import (
     QApplication,
+    QGridLayout,
     QLabel,
     QLineEdit,
     QMainWindow,
     QPushButton,
-    QGridLayout,
     QWidget,
 )
 
 
 # Subclass QMainWindow to customize your application's main window
 class MainWindow(QMainWindow):
-
     def __init__(self):
-        super(MainWindow, self).__init__()
+        super().__init__()
 
-        self.setWindowTitle("Widgets App")
-        self.setContentsMargins(24, 24, 24, 24)  # NEW - adds margin
+        self.setWindowTitle("Avatar-inator")
+        self.resize(320, 240)
+
+        # Initialize Widgets
+        title_label = QLabel("Avatar-inator",
+                             alignment=Qt.AlignmentFlag.AlignHCenter)
+
+        self.avatar_main_path = "resources/images/bottts_avatar.svg"
+        self.avatar_main = QSvgWidget(self.avatar_main_path)
+
+        # Seed input
+        self.seed_input = QLineEdit("")
+        self.seed_input.setPlaceholderText("Add text to customize")
+
+        # Get avatar button
+        self.get_avatar_button = QPushButton("Get Avatar")
+
+        # Avatar icons
+        # TODO: Turn these into custom widgets and set size
+        self.pixel_icon = QSvgWidget("resources/images/pixel_art_avatar.svg")
+        self.adventurer_icon = QSvgWidget("resources/images/adventurer_avatar.svg")
+        self.bottts_icon = QSvgWidget("resources/images/bottts_avatar.svg")
+        self.croodles_icon = QSvgWidget("resources/images/croodles_avatar.svg")
+        self.identicon_icon = QSvgWidget("resources/images/identicon_avatar.svg")
+        self.lorelei_icon = QSvgWidget("resources/images/lorelei_avatar.svg")
+        self.rings_icon = QSvgWidget("resources/images/rings_avatar.svg")
+        self.shapes_icon = QSvgWidget("resources/images/shapes_avatar.svg")
 
         layout = QGridLayout()
 
-        # Title
-        title_label = QLabel("Avatar-inator")
-
-        # Main Avatar
-        self.avatar_main_svg = QSvgWidget("resources/images/botts_avatar.svg")
-
-        # Let user choose a text seed
-        self.seed_input = QLineEdit("")
-
-        self.get_avatar_button = QPushButton("Get Avatar")
-
-        # Icon button widgets
-        pixel_slot_layout = controller.get_icon_layout("lorelei")
-        adventurer_slot_layout = controller.get_icon_layout("identicon")
-
-        # Add widgets to the layout
+        # Insert widgets into the layout
         layout.addWidget(title_label, 0, 0, 1, 4)
-        layout.addWidget(self.avatar_main_svg, 1, 1, 2, 2)
-        layout.addWidget(self.seed_input, 3, 0, 1, 3)
-        layout.addWidget(self.get_avatar_button, 3, 3, 1, 1)
-        layout.addLayout(pixel_slot_layout, 4, 0, 1, 1)
-        layout.addLayout(adventurer_slot_layout, 4, 1, 1, 1)
+        layout.addWidget(self.avatar_main, 1, 1, 1, 2,
+                         alignment=Qt.AlignmentFlag.AlignHCenter)
+        layout.addWidget(self.seed_input, 2, 0, 1, 3)
+        layout.addWidget(self.get_avatar_button, 2, 3, 1, 1)
+        layout.addWidget(self.pixel_icon, 3, 0)
+        layout.addWidget(self.adventurer_icon, 3, 1)
+        layout.addWidget(self.bottts_icon, 3, 2)
+        layout.addWidget(self.croodles_icon, 3, 3)
+        layout.addWidget(self.identicon_icon, 4, 0)
+        layout.addWidget(self.lorelei_icon, 4, 1)
+        layout.addWidget(self.rings_icon, 4, 2)
+        layout.addWidget(self.shapes_icon, 4, 3)
 
         widget = QWidget()
         widget.setLayout(layout)
+
+        # Set the central widget of the Window. Widget will expand
+        # to take up all the space in the window by default.
         self.setCentralWidget(widget)
-
-
-class Color(QWidget):
-    def __init__(self, color):
-        super(Color, self).__init__()
-        self.setAutoFillBackground(True)
-
-        palette = self.palette()
-        palette.setColor(QPalette.ColorRole.Window, QColor(color))
-        self.setPalette(palette)
 
 
 app = QApplication(sys.argv)
